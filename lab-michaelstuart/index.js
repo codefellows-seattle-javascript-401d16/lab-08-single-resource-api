dc48c09e9ae0fd8537a83090b4c5ce4de8046d0c
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-21 16:17:01 -0700

    mvp achieved, ned to refactor and implement tests

---
+++ lab-michaelstuart/index.js
@@ -0,0 +1,5 @@
+
+'use strict';
+
+const server = require('./lib/server.js');
+server.listen(8080, () => console.log('server listening on port 8080'));
---
+++ lab-michaelstuart/lib/user.js
@@ -0,0 +1,6 @@
+module.exports = function User(name, password, email, id) {
+  this.name = name;
+  this.password = password;
+  this.email = email;
+  this.id = id;
+};
