3925e691a87ddccf942779eb6c9bb1b92aa342df
janousek77 <janousek77@gmail.com>
2017-06-21 14:27:29 -0700

    skeleton and demo code added

---
+++ lab-chris/lib/server.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const http = require('http');
+const router = require('./router.js');
+const uuid = require('uuid');
+
+var storage = {};
+
+router.get('/hello', (req, res) => {
+  res.write('yeyyeyeyye');
+  res.end();
+});
+
+router.post('/api/notes', (req, res) => {
+  console.log('hit /api/notes');
+  if(!req.body.content){
+    res.write(400);
+    res.end();
+    return;
+  }
+
+  let note = {
+    id: uuid.v1(),
+    content: req.body.content,
+  };
+
+  storage[note.id] = note;
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+  res.write(JSON.stringify(note));
+  res.end();
+});
+
+router.get('/api/notes', (req, res) => {
+  if(!req.url.query.id){
+    res.writeHead(400);
+    res.end();
+    return ;
+  }
+
+  if(!storage[req.url.query.id]){
+    res.writeHead(404);
+    res.end();
+    return ;
+  }
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+
+  res.write(JSON.stringify(storage[req.url.query.id]));
+  res.end();
+
+});
+
+const server = module.exports = http.createServer(router.route);
