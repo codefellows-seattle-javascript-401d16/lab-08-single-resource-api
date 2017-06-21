515310a074e7bebb4ddb9ab42ba2a20adef134a7
Matthew Parker <cadburylion@outlook.com>
2017-06-21 14:42:18 -0700

    Actually finsihed scaffold and got server to run

--- lab-matthew/index.js
+++ lab-matthew/index.js
@@ -1,4 +1,4 @@
 'use strict';
 
 const server = require('./lib/server.js');
-server.listen(333, () => console.log('server up on 3000'));
+server.listen(3000, () => console.log('server up on 3000'));
