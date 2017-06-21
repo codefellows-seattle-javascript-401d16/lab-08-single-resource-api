12ea7921e4d77781411a8e785e0b48ce75a2cded
Scott <sdmccoy@gmail.com>
2017-06-21 16:27:03 -0700

    build object constructor in the profile-construcjer.js file

--- lab8-scott/lib/server.js
+++ lab8-scott/lib/server.js
@@ -1,7 +1,32 @@
 'use strict';
 
 const http = require('http');
+const uuid = require('uuid');
 
+let climberPool = {};
+
+router.post(`/api/climbers`, (req, res)=>{
+  if(!req.body.age && !req.body.style) {
+    res.writeHead(400);
+    res.end();
+    return;
+  }
+  //start of my object constructor on POST requests
+  let climberProfile = {
+    id: uuid.v1(),
+    age: req.body.age,
+    style: req.body.style,
+  };
+  //putting the new profile object from the POST request in to the climberpool object
+  climberPool[climberProfile] = climberProfile;
+  //respond that we recieved the request
+  res.writeHead(200, {
+    'Content-Type' : 'application/json',
+  });
+  res.write(JSON.stringify(climberProfile));
+  res.end();
+  return;
+});
 
 
 
