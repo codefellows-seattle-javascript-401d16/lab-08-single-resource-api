afea49e33dff258122bb2a8820d1a36afd0eee50
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-21 23:31:22 -0700

    finish readme and tests

--- lab-michaelstuart/lib/server.js
+++ lab-michaelstuart/lib/server.js
@@ -9,29 +9,29 @@ const headWrite = require('./head-write');
 
 router.post('/api/user', (req, res) => {
   const { name, password, email } = req.body;
-  if (!name || !password || ! email) return headWrite(res, 400);
+  if (!name || !password || ! email) return headWrite(res, 400, 'bad request');
   const user = new User(name, password, email, uuid.v1());
   database[user.id] = user;
-  headWrite(res, 201, JSON.stringify(user));
+  headWrite(res, 201, user);
 });
 
 router.get('/api/user', (req, res) => {
   const id = req.url.query.id;
-  if (id && !database[id]) return headWrite(res, 404);
+  if (id && !database[id]) return headWrite(res, 404, 'not found');
   const userList = (Object.keys(database) || []).map(v => v);
-  headWrite(res, 200, JSON.stringify(id ? database[id] : userList));
+  headWrite(res, 200, id ? database[id] : userList);
 });
 
 router.put('/api/user', (req, res) => {
   const id = req.url.query.id || req.body.id;
-  if (!database[id]) return headWrite(res, 400);
+  if (!database[id]) return headWrite(res, 400, 'bad request');
   database[id] = Object.assign({}, database[id], req.body);
-  headWrite(res, 200, `user ${name} successfully updated`);
+  headWrite(res, 202, database[id]);
 });
 
 router.delete('/api/user', (req, res) => {
   const id = req.url.query.id;
-  if (!database[id]) return headWrite(res, 400);
+  if (!database[id]) return headWrite(res, 404, 'not found');
   delete database[id];
   headWrite(res, 204);
 });
--- lab-michaelstuart/test/server-test.js
+++ lab-michaelstuart/test/server-test.js
@@ -3,41 +3,116 @@
 const expect = require('expect');
 const superagent = require('superagent');
 const server = require('../lib/server.js');
-// const database = require('../model/database');
+const database = require('../model/database');
+
+let id = '';
 
 describe('testing http methods', () => {
   before(done => server.listen(3000, () => done()));
   after(done => server.close(() => done()));
 
   describe('testing POST route', () => {
-    it('should return code 201 when request body valid', (done) => {
+    it('should return 400 status and bad request string when request body invalid', (done) => {
+      superagent.post('localhost:3000/api/user')
+        .send({})
+        .end((err, res) => {
+          expect(res.text).toBe('bad request');
+          expect(res.status).toEqual(400);
+          done();
+        });
+    });
+    it('should return 201 status when request body valid', (done) => {
       superagent.post('localhost:3000/api/user')
         .send({ name: 'michael', password: 'abcd', email: 'michael@stuart.com'})
         .end(((err, res) => {
-          if (err) return done(err);
+          id = Object.keys(database)[0];
+          expect(id).toExist();
           expect(res.status).toEqual(201);
           done();
         }));
     });
-    it('should return code 400 when request body invalid', (done) => {
-      superagent.post('localhost:3000/api/user')
-        .end((err) => {
-          expect(err.status).toEqual(400);
+  });
+
+  describe('testing GET route', () => {
+    it('should return 404 status and not found string when query invalid', (done) => {
+      superagent.get('localhost:3000/api/user?id=invalid')
+        .end((err, res) => {
+          expect(res.text).toBe('not found');
+          expect(res.status).toEqual(404);
+          done();
+        });
+    });
+    it('should return 200 status and user id array when query undefined', (done) => {
+      superagent.get('localhost:3000/api/user')
+        .end((err, res) => {
+          expect(typeof JSON.parse(res.text)[0]).toBe('string');
+          expect(res.status).toEqual(200);
+          done();
+        });
+    });
+    it('should return 200 status and user object when query valid', (done) => {
+      superagent.get(`localhost:3000/api/user?id=${id}`)
+        .end((err, res) => {
+          expect(JSON.parse(res.text).id).toBe(id);
+          expect(res.status).toEqual(200);
           done();
         });
     });
   });
 
-  describe('testing GET route', () => {
-    it('should return array of all user ids when query undefined', (done) => {
-      superagent.get('localhost:3000/api/user')
-        .query(null)
+  describe('testing PUT route', () => {
+    it('should return 404 status and not found string when query invalid', (done) => {
+      superagent.put('localhost:3000/api/user?id=invalid')
+        .send({})
         .end((err, res) => {
+          expect(res.text).toBe('bad request');
+          expect(res.status).toEqual(400);
+          done();
+        });
+    });
+    it('should return 404 status and not found string when request body invalid', (done) => {
+      superagent.put('localhost:3000/api/user')
+        .send({ id: 'invalid' })
+        .end((err, res) => {
+          expect(res.text).toBe('bad request');
+          expect(res.status).toEqual(400);
+          done();
+        });
+    });
+    it('should return 202 status and user object when request body valid', (done) => {
+      superagent.put('localhost:3000/api/user')
+        .send({ id, name: 'mike' })
+        .end(((err, res) => {
+          expect(database[id].name).toBe('mike');
+          expect(res.status).toEqual(202);
+          done();
+        }));
+    });
+    it('should return 202 status and user object when query valid', (done) => {
+      superagent.put(`localhost:3000/api/user?id=${id}`)
+        .send({ name: 'mike' })
+        .end(((err, res) => {
+          expect(database[id].name).toBe('mike');
+          expect(res.status).toEqual(202);
+          done();
+        }));
+    });
+  });
 
-          // TODO figure out why res.body is {} instead of ['id_placeholder']
-
-          console.log(res.body);
-          expect(res.status).toEqual(200);
+  describe('testing DELETE route', () => {
+    it('should return 404 status and not found string when query invalid', (done) => {
+      superagent.delete('localhost:3000/api/user?id=invalid')
+        .end((err, res) => {
+          expect(res.text).toBe('not found');
+          expect(res.status).toEqual(404);
+          done();
+        });
+    });
+    it('should return 204 status and user object when query valid', (done) => {
+      superagent.delete(`localhost:3000/api/user?id=${id}`)
+        .end((err, res) => {
+          expect(database[id]).toNotExist();
+          expect(res.status).toEqual(204);
           done();
         });
     });
