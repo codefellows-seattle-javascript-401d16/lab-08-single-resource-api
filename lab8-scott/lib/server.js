4a6a1a9d14cdc0711ade4ca0e5ec4838ce0db718
Scott <sdmccoy@gmail.com>
2017-06-21 21:46:55 -0700

    route logic is written for all post, get, delete, and put methods.

--- lab8-scott/lib/server.js
+++ lab8-scott/lib/server.js
@@ -2,34 +2,93 @@
 
 const http = require('http');
 const router = require('./router.js');
-const Profileconstructor = require('../model/profile-constructor.js');
+const ProfileConstructor = require('../model/profile-constructor.js');
 
 let climberPool = {};
 
-router.post(`/api/climbers`, (req, res)=>{
+//start of the route for Create/Post. Start with create so we have something in the db to work with.
+router.post(`/api/climberprofile`, (req, res)=>{
   if(!req.body.age && !req.body.style) {
     res.writeHead(400);
+    res.write(`Please try this format {age: 27, style: trad}`);
     res.end();
     return;
   }
   //creating a new profile based on the parsed body from the post request
-  let newClimberProfile = new Profileconstructor(req.body.age, req.body.style);
+  let climberProfile = new ProfileConstructor(req.body.age, req.body.style);
   //putting the new profile id value as the key in the climberPool and set the new Profile as the value to that key id #
-  climberPool[newClimberProfile.id] = newClimberProfile;
+  climberPool[climberProfile.id] = climberProfile;
   //respond that we recieved the request
   res.writeHead(200, {
     'Content-Type' : 'application/json',
   });
-  res.write(JSON.stringify(newClimberProfile));
+  res.write(JSON.stringify(climberProfile));
   res.end();
   return;
 });
 
-//creating routes for Create/Post Read/Get and Delete/delete
-router.post(`/api/climberprofile`, (req, res) =>{
+//creating route for Read/Get
+router.get(`/api/climberprofile`, (req, res) =>{
+  if (!climberPool[req.url.query]) {
+    res.writeHead(404);
+    res.write(`No profile found with that id`);
+    res.end();
+    return;
+  }
+  if (!req.url.query) {
+    res.writeHead(400);
+    res.write(`Please input an id`);
+    res.end();
+    return;
+  }
   res.writeHead(200, {
-    /////continue on
+    'Content-Type' : 'application/json',
   });
+  res.write(`Here is the requested profile\n ${climberPool[req.url.query]}`);
+  res.end();
+  return;
+});
+
+//creating route for DELETE
+router.delete(`/api/climberprofile`, (req, res) =>{
+  if (!climberPool[req.url.query]) {
+    res.writeHead(404);
+    res.write(`No profile was found with that id`);
+    res.end();
+    return;
+  }
+  //delete the profile with matching the id
+  delete climberPool[req.url.query];
+  res.writeHead(204, {
+    'Content-Type' : 'application/json',
+  });
+  res.write(`You deleted profile ${req.url.query}`);
+  res.end();
+  return;
+});
+
+//creating route for Put
+router.get(`/api/climberprofile`, (req, res) =>{
+  if (!climberPool[req.url.query]) {
+    res.writeHead(404);
+    res.write(`No profile found with that id`);
+    res.end();
+    return;
+  }
+  if (!req.url.query) {
+    res.writeHead(400);
+    res.write(`Please input an id`);
+    res.end();
+    return;
+  }
+  //data is passed in the body request as stringified json. Set the new req body to the profile at that id.
+  climberPool[req.url.query] = req.body;
+  res.writeHead(200, {
+    'Content-Type' : 'application/json',
+  });
+  res.write(climberPool[req.url.query]);
+  res.end();
+  return;
 });
 
 
--- lab8-scott/model/profile-constructor.js
+++ lab8-scott/model/profile-constructor.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const uuid = require('uuid');
+const uuid = require('uuid/v1');
 
 //start of my object constructor on POST requests
 module.exports = function(age, style) {
