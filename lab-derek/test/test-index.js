14f4e1b0fe6645b30489fe905643625e1f46c44d
derekcfarmer <derek.farmer@gmail.com>
2017-06-22 10:51:50 -0700

    fixed typos

--- lab-derek/test/test-index.js
+++ lab-derek/test/test-index.js
@@ -31,12 +31,12 @@ describe('testing task routes', () => {
   });
 
 
-  //Write tests to ensure your /api/simple-resource-name endpoint responds as described for each condition below:
+  //Write tests to ensure your /api/tasks/simple-resource-name endpoint responds as described for each condition below:
 
   //TODO: GET - test 404, responds with 'not found' for valid request made with an id that was not found
 
   it('should respond with "not found" for valid requests with unknown id', () => {
-    superagent.get('/api?id=test')
+    superagent.get('/api/tasks?id=test')
       .end((err, res) => {
         expect(res.status).toEqual(404);
         expect(res.body).toEqual('not found');
@@ -49,7 +49,7 @@ describe('testing task routes', () => {
   //TODO: POST - test 201, response body like {<data>} for a post request with a valid body
 
   it('should POST a new object and return it, then return a status code 200 and valid data if valid id is provided in query string', () => {
-    superagent.post('localhost:3000/api')
+    superagent.post('localhost:3000/api/tasks')
       .send(
         {
           "taskName": "test","xp": "50","id": "10",
@@ -62,7 +62,7 @@ describe('testing task routes', () => {
           }
         );
       });
-    superagent.get('localhost:3000/api?=10')
+    superagent.get('localhost:3000/api/tasks?=10')
       .end((err, res) => {
         expect(res.status).toEqual(200);
         expect(res.body).toEqual(
@@ -73,10 +73,10 @@ describe('testing task routes', () => {
       });
   });
 
-  //TODO: GET - test 400, responds with 'bad request' for if no id is provided in the query string  BONUS: 2pts a GET request to /api/simple-resource-name with no ?id= should return an array of all of the ids for that resource if you do this you dont have to test 400 to GET /api/simple-resource-name
+  //TODO: GET - test 400, responds with 'bad request' for if no id is provided in the query string  BONUS: 2pts a GET request to /api/tasks/simple-resource-name with no ?id= should return an array of all of the ids for that resource if you do this you dont have to test 400 to GET /api/tasks/simple-resource-name
 
   it('should return a status code 400 and array of available IDs if no id is provided in query string', () => {
-    superagent.get('localhost:3000/api')
+    superagent.get('localhost:3000/api/tasks')
       .end((err, res) => {
         expect(res.status).toEqual(400);
         expect(res.body).toEqual('bad request');
@@ -87,7 +87,7 @@ describe('testing task routes', () => {
   //TODO: POST - test 400, responds with 'bad request' for if no body provided or invalid body
 
   it('should responds with status 400 and "bad request" for  invalid body', () => {
-    superagent.post('localhost:3000/api')
+    superagent.post('localhost:3000/api/tasks')
       .send(
         {}
       ).end((err, res) => {
@@ -100,7 +100,7 @@ describe('testing task routes', () => {
   //TODO: PUT - test 400, responds with 'bad request' for if no body provided or invalid body
 
   it('should responds with status 400 and "bad request" for invalid body', () => {
-    superagent.put('localhost:3000/api')
+    superagent.put('localhost:3000/api/tasks')
       .send(
         {}
       ).end((err, res) => {
@@ -113,7 +113,7 @@ describe('testing task routes', () => {
   //TODO: PUT - test 202, response body like {<data>} for a put request with a valid id
 
   it('should PUT to an object and return the updated object, then return a status code 202 and valid data if valid id is provided in query string', () => {
-    superagent.post('localhost:3000/api')
+    superagent.post('localhost:3000/api/tasks')
       .send(
         {
           "taskName": "test","xp": "50","id": "10",
@@ -126,7 +126,7 @@ describe('testing task routes', () => {
           }
         );
       });
-    superagent.put('localhost:3000/api')
+    superagent.put('localhost:3000/api/tasks')
       .send(
         {
           "xp": "500","id": "10",
@@ -145,7 +145,7 @@ describe('testing task routes', () => {
   //TODO: DELETE - test 404, responds with 'not found' for valid request made with an id that was not found
 
   it('should return a status code 404 if valid id not found', () => {
-    superagent.delete('localhost:3000/api?id=9999')
+    superagent.delete('localhost:3000/api/tasks?id=9999')
       .end((err, res) => {
         expect(res.status).toEqual(404);
         expect(res.body).toEqual('not found');
@@ -156,7 +156,7 @@ describe('testing task routes', () => {
   //TODO: DELETE - test 204, response for a delete request with a valid id
 
   it('should return a status code 404 if valid id not found', () => {
-    superagent.post('localhost:3000/api')
+    superagent.post('localhost:3000/api/tasks')
       .send(
         {
           "taskName": "test","xp": "50","id": "10",
@@ -169,12 +169,12 @@ describe('testing task routes', () => {
           }
         );
       });
-    superagent.delete('localhost:3000/api?id=10')
+    superagent.delete('localhost:3000/api/tasks?id=10')
       .end((err, res) => {
         expect(res.status).toEqual(204);
         done();
       });
-    superagent.get('localhost:3000/api?id=10')
+    superagent.get('localhost:3000/api/tasks?id=10')
       .end((err, res) => {
         expect(res.status).toEqual(404);
         expect(res.body).toEqual('not found');
