461519061610c9fbb2839204bdc6868a42fb312e
Michael Axelson <axelmichael11@gmail.com>
2017-06-22 10:09:16 -0700

    get statement is kind of working

--- lab-michael-axelson/lib/server.js
+++ lab-michael-axelson/lib/server.js
@@ -35,8 +35,8 @@ router.post('/newUser', (req, res) => {
   let user = new User(req.body.title, req.body.content);
   storage[user.title] = user;// storage[user.id] = user;
   console.log(req.body);
-  console.log(req.url);
-  console.log(storage);
+  // console.log(req.url);
+  console.log(Object.keys(storage)[0]);
   res.writeHead(200, {
     'Content-Type': 'application/json',
   });
@@ -52,37 +52,68 @@ router.get('/readTitles', (req, res) => {
     res.end();
     return;
   }
-  console.log(storage);
+  res.write(JSON.stringify(storage));
   let titleInfo = [];
+  console.log(storage); //LEFT OFF AROUND HERE
   for (var i=0; i< storage.length; i++){
-    if (req.body.title===Object.keys(storage)[i]) {
+    if (req.body.title===storage[req.body.title[i]]) {
       console.log('almost!!!');
-      titleInfo.push(storage.title[i]);
+      titleInfo.push(storage[i]);
     } else {console.log('almost!!');}
   }
   if (titleInfo===[]) {
     res.write('these are all of the titles that matched your request...\n'+JSON.stringify(titleInfo));
   } else {
-    res.write('no user with that information!!');
+    res.write('no title with that information!!');
   }
   res.end();
 });
 
-router.delete('/deleteTitle', (req,res) => {
+router.delete('/deleteUser', (req,res) => {
   console.log(storage);
   if(!req.body){ // if(!req.body.content){
     res.writeHead(400);
     res.end();
     return;
   }
+  console.log('im inside of delete now');
+  console.log(Object.keys(storage)[0]);
+  console.log(req.body.title);
+  console.log(JSON.stringify(req.body.title));
+  res.write(JSON.stringify(Object.keys(storage)[0]));
+  let numDeleted = 0;
   for (var i=0; i< storage.length; i++){
-    if (req.body.title===storage.title[i]) {
-      titleInfo.push(storage.title[i]);
+    if (req.body.title===Object.keys(storage)[i]) {
+      delete storage[storage.title[i]];
+      numDeleted+=1;
     }
   }
+  res.write(JSON.stringify('You deleted '+numDeleted+' users! post some more stuff... :)'));
+  res.end();
+});
 
-
-})
+router.delete('/deleteUser', (req,res) => {
+  console.log(storage);
+  if(!req.body){ // if(!req.body.content){
+    res.writeHead(400);
+    res.end();
+    return;
+  }
+  console.log('im inside of delete now');
+  console.log(Object.keys(storage)[0]);
+  console.log(req.body.title);
+  console.log(JSON.stringify(req.body.title));
+  res.write(JSON.stringify(Object.keys(storage)[0]));
+  let numDeleted = 0;
+  for (var i=0; i< storage.length; i++){
+    if (req.body.title===Object.keys(storage)[i]) {
+      delete storage[storage.title[i]];
+      numDeleted+=1;
+    }
+  }
+  res.write(JSON.stringify('You deleted '+numDeleted+' users! post some more stuff... :)'));
+  res.end();
+});
 
 
 
@@ -101,28 +132,28 @@ router.get('/newSoup', (req, res) => {
 
 
 
-router.get('/api/notes', (req, res) => {
-  if(!req.url.query.id){
-    res.writeHead(400);
-    res.end();
-    return ;
-  }
+// router.get('/api/notes', (req, res) => {
+//   if(!req.url.query.id){
+//     res.writeHead(400);
+//     res.end();
+//     return ;
+//   }
+//
+//   if(!storage[req.url.query.id]){
+//     res.writeHead(404)
+//     console.log('suhh dude');
+//     res.end();
+//     return ;
+//   }
+//
+//   res.writeHead(200, {
+//     'Content-Type': 'application/json',
+//   });
 
-  if(!storage[req.url.query.id]){
-    res.writeHead(404)
-    console.log('suhh dude');
-    res.end();
-    return ;
-  }
-
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
-
-  res.write(JSON.stringify(storage[req.url.query.id]));
-  res.end();
-
-});
+//   res.write(JSON.stringify(storage[req.url.query.id]));
+//   res.end();
+//
+// });
 
 // create server
 const server = module.exports = http.createServer(router.route);
