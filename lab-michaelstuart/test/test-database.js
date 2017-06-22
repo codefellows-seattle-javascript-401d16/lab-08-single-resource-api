9a9a1914fc19b9765a9a682a63c23589c1b102a7
Michael Stuart <mike.w.stuart@gmail.com>
2017-06-21 21:50:15 -0700

    finish post tests

---
+++ lab-michaelstuart/test/test-database.js
@@ -0,0 +1,20 @@
+module.exports = {
+  one: {
+    name: 'a',
+    password: 'b',
+    email: 'c',
+    id: 'one',
+  },
+  two: {
+    name: 'd',
+    password: 'e',
+    email: 'f',
+    id: 'two',
+  },
+  three: {
+    name: 'g',
+    password: 'h',
+    email: 'i',
+    id: 'three',
+  },
+};
