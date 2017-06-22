409ae4222a4136de2fd4653fd13188fd7536b0cd
derekcfarmer <derek.farmer@gmail.com>
2017-06-22 09:35:07 -0700

    bonus test working

--- lab-derek/test/test-index.js
+++ lab-derek/test/test-index.js
@@ -44,17 +44,6 @@ describe('testing task routes', () => {
       });
   });
 
-  //TODO: GET - test 400, responds with 'bad request' for if no id is provided in the query string  BONUS: 2pts a GET request to /api/simple-resource-name with no ?id= should return an array of all of the ids for that resource if you do this you dont have to test 400 to GET /api/simple-resource-name
-
-  it('should return a status code 400 if no id is provided in query string', () => {
-    superagent.get('localhost:3000/api')
-      .end((err, res) => {
-        expect(res.status).toEqual(400);
-        expect(res.body).toEqual('bad request');
-        // expect(res.body)
-        done();
-      });
-  });
 
   //TODO: GET - test 200, response body like {<data>} for a request made with a valid id
   //TODO: POST - test 201, response body like {<data>} for a post request with a valid body
@@ -84,6 +73,17 @@ describe('testing task routes', () => {
       });
   });
 
+  //TODO: GET - test 400, responds with 'bad request' for if no id is provided in the query string  BONUS: 2pts a GET request to /api/simple-resource-name with no ?id= should return an array of all of the ids for that resource if you do this you dont have to test 400 to GET /api/simple-resource-name
+
+  it('should return a status code 400 and array of available IDs if no id is provided in query string', () => {
+    superagent.get('localhost:3000/api')
+      .end((err, res) => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual('bad request');
+        expect(res.body).toEqual(['10']);
+        done();
+      });
+  });
   //TODO: POST - test 400, responds with 'bad request' for if no body provided or invalid body
 
   it('should responds with status 400 and "bad request" for  invalid body', () => {
